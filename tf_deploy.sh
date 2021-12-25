6e515adda5858aa20cdb9f8e7255cc80e9c32bd1
Fabio F Silva <ffsilva@outlook.com>
2021-12-25 03:22:38 +0000

    added an alias to terraform using tf instead

--- tf_deploy.sh
+++ tf_deploy.sh
@@ -5,3 +5,5 @@ sudo apt-get update && sudo apt-get install -y gnupg software-properties-common 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
+echo "alias tf='terraform'" >> ~/.bashrc
+alias tf='terraform'
