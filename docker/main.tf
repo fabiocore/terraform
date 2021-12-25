b169d89f3029508e3772852ba03732f07211bf43
Fabio F Silva <ffsilva@outlook.com>
2021-12-25 19:58:00 +0000

    formatted all notes, comments and help

--- docker/main.tf
+++ docker/main.tf
@@ -1,17 +1,26 @@
 /*
 
-Notes:
+notes 
 
-- Remember: Everytime you add a new resource you need to do '$terraform init'
-- You can run '$terraform fmt' to format the document or
-  '$terraform fmt -diff' to format and see what is changed.
-  Alternativelly you can use auto indent and auto format in VSCode:
-  https://linuxpip.org/auto-indent-vscode/ 
-- Join functions can be use for concatenating elements
-  join(separator, list)
-  join(", ", ["foo", "bar", "baz"]) # result> foo, bar, baz
+- commands:
+    terraform init => to initialize
+    terraform plan => to evaluate the configurations
+    terraform apply => to deploy, and use '--auto-approve' to skip interactive approval
+    terraform fmt => to format the .tf document, and use '-diff' to see the differences
+    terraform show => to see the configurations in the tfstate. tip: forward the result to grep or jq
+- functions:
+    join
+        join(separator, list)
+        join(", ", ["foo", "bar", "baz"]) # result> foo, bar, baz
+        https://www.terraform.io/language/functions/join
+- tips:
+    Remember: Everytime you add a new resource you need to do '$terraform init'
+    Use auto indent and auto format in VSCode:
+    https://linuxpip.org/auto-indent-vscode/ 
 
 */
+
+# using docker provider
 
 terraform {
   required_providers {
@@ -24,26 +33,25 @@ terraform {
 
 provider "docker" {}
 
-# Use the random resource provider to gerate random string, numbers or other
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+/*
+  random_string
+  Use the random resource provider to gerate random string, numbers or other
+  In the code below it's generating a single random string that can be used like => random_string.random.result
+  https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+*/
+
 resource "random_string" "random" {
   length  = 4
   special = false
   upper   = false
 }
 
-resource "random_string" "random2" {
-  length  = 4
-  special = false
-  upper   = false
-}
-
-# Pulls the image
+# pulls the image
 resource "docker_image" "nodered" {
   name = "nodered/node-red:latest"
 }
 
-# Create a container
+# create a container using join and random_string
 resource "docker_container" "nodered" {
   image = docker_image.nodered.latest
   name  = join("-", ["nodered", random_string.random.result])
@@ -53,35 +61,23 @@ resource "docker_container" "nodered" {
   }
 }
 
-resource "docker_container" "nodered2" {
-  image = docker_image.nodered.latest
-  name  = join("-", ["nodered", random_string.random2.result])
-  ports {
-    internal = 1880
-    # external = 1880
-  }
-}
+/*
+  output
+  Use output to export/expose a value after the deployment
+  Examples:
+    docker_container.nodered.name
+    docker_container.nodered.ip_address
+    docker_container.nodered.ports[0].external
+  https://www.terraform.io/language/values/outputs
+*/
 
-# Use output value to expose/explort information after deploying a resource
-# https://www.terraform.io/language/values/outputs
+# output the container name
 output "container_name" {
   value = docker_container.nodered.name
 }
 
-output "container_ip_addr" {
-  value = docker_container.nodered.ip_address
-}
-
-output "container_ext_port" {
-  value = docker_container.nodered.ports[0].external
-}
-
+# using join to output the ip + external port
 output "container_url" {
   value       = join(":", [docker_container.nodered.ip_address, docker_container.nodered.ports[0].external])
   description = "Nodered container #1 URL"
-}
-
-output "container2_url" {
-  value       = join(":", [docker_container.nodered.ip_address, docker_container.nodered2.ports[0].external])
-  description = "Nodered container #2 URL"
 }
