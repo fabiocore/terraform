2e91e11caf345a379560870120a27558281ce4bc
Fabio F Silva <ffsilva@outlook.com>
2022-01-03 20:36:22 +0000

    finished the terraform lab

--- lab1/main.tf
+++ lab1/main.tf
@@ -9,15 +9,30 @@ terraform {
 
 provider "docker" {}
 
+variable "int_port" {
+  default = 3000
+  validation {
+    condition     = var.int_port == 3000
+    error_message = "The frana port must be set to 3000."
+  }
+}
+
+variable "ext_port" {}
+
 resource "docker_image" "grafana_image" {
   name = "grafana/grafana"
 }
 
 resource "docker_container" "grafana_container" {
+  count = 2
   image = docker_image.grafana_image.latest
-  name  = "grafana"
+  name  = "grafana-${count.index}"
   ports {
-    internal = 3000
-    external = 3000
+    internal = var.int_port
+    external = var.ext_port[count.index]
   }
+}
+
+output "public_ip" {
+  value = [for x in docker_container.grafana_container : "${x.name} - ${x.ip_address}:${x.ports[0].external}"]
 }
